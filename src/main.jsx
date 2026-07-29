11752d59cd4babffbad17600903f165ecf9e722a
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-29 15:38:59 +0300

    Dispatching Action from React Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { store } from "./redusx/store";
+import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
