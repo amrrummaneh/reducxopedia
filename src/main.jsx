69530a57b042412e09b3f932da095df259f6d455
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-29 14:55:08 +0300

    Add Reduxe

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="d-flex flex-column min-vh-100">
-      <Header />
-      <main className="flex-grow-1">Main Content</main>
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
+        <main className="flex-grow-1">Main Content</main>
+        <Footer />
+      </div>
+    </Provider>
   </StrictMode>,
 );
