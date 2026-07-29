8a7ae202293ca12c95d6ea3f8a37d0cafabcc2ce
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-29 16:08:18 +0300

    Add Destination Slice

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import Counter from "./components/Counter";
+import DestinationList from "./components/DestinationList";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -14,6 +15,12 @@ createRoot(document.getElementById("root")).render(
         <main className="flex-grow-1 row">
           <div className="col-12">
             <Counter />
+            <div className="p-3 m-4 border">
+              <h4 className="text-success pb-4 text-center">
+                Destination List
+              </h4>
+              <DestinationList />
+            </div>
           </div>
         </main>
         <Footer />
--- src/redux/slice/counterSlice.js
+++ src/redux/slice/counterSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { count: 10 };
 
-export const counterSlice = createSlice({
+const counterSlice = createSlice({
   name: "counter",
   initialState: initialState,
   reducers: {
